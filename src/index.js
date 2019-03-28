32550ca4293aadb67be38a5d0e1c16933e069ca2
e-liza <eliza.lks@gmail.com>
2019-03-28 19:58:10 +0300

    Adding app structure and adding needed imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './components/app/app';
+import ErrorBoundry from './components/error-boundry/error-boundry';
+import BookStoreService from './services/bookstore-service';
+import { BookstoreServiceProvider } from './components/bookstore-service-context/bookstore-service-context';
+import store from './store';
+
+const bookStoreService = new BookStoreService();
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundry>
+      <BookstoreServiceProvider value={bookStoreService}>
+        <Router>
+          <App />
+        </Router>
+      </BookstoreServiceProvider>
+    </ErrorBoundry>
+  </Provider>,
+  rootElement
+);
